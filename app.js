1892e2a537a4da056660a5fab4a5a3efc8aafdb7
Tomac Barin Jansson <tomacbarin@me.com>
2025-10-23 11:27:36 +0200

    Första etapp klar.

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 // Hämta skämt från API
+const jokeHtml = document.getElementById("joke");
+const jokeBtnHtml = document.getElementById("jokeBtn");
+
+getJoke();
 
 // async-function
-
-// Behandla datan
-
-// Placera på sidan
+async function getJoke() {
+  jokeHtml.innerHTML = "Laddar skämt";
+  console.log("Laddar skämt");
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+  const response = await fetch(
+    "https://api.chucknorris.io/jokes/random",
+    config
+  );
+  // Behandla datan
+  const data = await response.json();
+  // Placera på sidan
+  jokeHtml.innerHTML = data.value;
+}
 
 // Knapp som hämtar och visar nytt skämt
+jokeBtnHtml.addEventListener("click", getJoke);
